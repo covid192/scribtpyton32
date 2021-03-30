581f2daaebcdeaf692697b3d3bd81662acd27538
ANIMOUS ISLAMIC <77113273+covid192@users.noreply.github.com>
2021-03-31 00:45:52 +0100

    Add files via upload

---
+++ animous.py
@@ -0,0 +1,12 @@
+#-*-coding:utf8;-*-
+#qpy:console
+
+print "This is console module"
+import base64
+M = base64.b64 encode( ' * ')
+N = base64.b64 encode( ' * ')
+m = input ( 'press 1 for encoder press 2 for decode : ' )
+if m==1
+print M
+elif m==2
+print N
